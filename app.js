574d7b2b7a96ff6d1ed6d2809572d4beed69c494
Alef Santiago <alefzell13@gmail.com>
2024-11-28 13:41:34 -0300

    PWA concluido

--- app.js
+++ app.js
@@ -5,22 +5,17 @@ myModal.addEventListener('shown.bs.modal', () => {
   myInput.focus()
 })
 
-// install.js
 let deferredPrompt;
 window.addEventListener('beforeinstallprompt', (e) => {
-  // Previne a janela de instalação automática
   e.preventDefault();
   deferredPrompt = e;
 
-  // Exibe um botão personalizado para o usuário
   const installButton = document.getElementById('installButton');
   installButton.style.display = 'block';
 
   installButton.addEventListener('click', () => {
-    // Exibe o prompt de instalação
     deferredPrompt.prompt();
 
-    // Espera a resposta do usuário
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
         console.log('Usuário aceitou a instalação');
