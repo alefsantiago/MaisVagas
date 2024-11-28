39484109ff677bf47e2bd12f087f689308e573d0
Alef Santiago <alefzell13@gmail.com>
2024-11-28 13:37:53 -0300

    PWA concluido

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       if (response) {
-        return response;  // Retorna do cache
+        return response; 
       }
 
     
