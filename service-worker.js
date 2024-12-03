f7f81350c77f34587b1d1f907190ae101fa2da05
Alef Santiago <alefzell13@gmail.com>
2024-12-03 19:45:21 -0300

    PWA concluido

--- service-worker.js
+++ service-worker.js
@@ -2,18 +2,18 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('vagas-cache').then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/app.js',
-        '/logo.png',
-        '/logo192x192.png',
-        '/logo512x512.png',
-        '/screenshot-mobile.png',
-        '/estagio1.jpg',   
-        '/estagio2.jpg',
-        '/estagio3.jpg',
-        '/estagio4.jpg'
+        './',
+        './index.html',
+        './styles.css',
+        './app.js',
+        './logo.png',
+        './logo192x192.png',
+        './logo512x512.png',
+        './screenshot-mobile.png',
+        './estagio1.jpg',   
+        './estagio2.jpg',
+        './estagio3.jpg',
+        './estagio4.jpg'
       ]);
     })
   );
